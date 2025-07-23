6f66c908132ab1095830bb1a0003bdf852e1c64c
Amro Alsharafi <amroalsharafi@gmail.com>
2025-07-23 13:51:02 +0800

    test: check if Skills component causes SSR error

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
 "use client";
 
 import { Hero } from "@/components/main/hero";
+import { Skills } from "@/components/main/skills";
 // import { Encryption } from "@/components/main/encryption";
 // import { Projects } from "@/components/main/projects";
-// import { Skills } from "@/components/main/skills";
 
 export default function Home() {
   return (
     <main className="h-full w-full">
       <div className="flex flex-col gap-20">
         <Hero />
-        {/* <Skills /> */}
+        <Skills />
         {/* <Encryption /> */}
         {/* <Projects /> */}
       </div>
